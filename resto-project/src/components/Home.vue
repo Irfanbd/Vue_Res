98487a0803ba82fc8440aa3e8d611afb6521ad64
Irfanbd <gourob83@gmail.com>
2021-12-25 07:11:29 +0600

    update

---
+++ resto-project/src/components/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>HEllO User, Welcome</h1>
+</template>
+<script>
+export default {
+  name: "Home",
+};
+</script>
--- resto-project/src/components/SignUp.vue
+++ resto-project/src/components/SignUp.vue
@@ -1,77 +1,68 @@
 <template>
-<img class="logo" src="../assets/logo.jpg" alt="">
-    <h1>SignUp</h1>
-    <div class="container">
-
-<div class="register">
-<input type="text" v-model="name" placeholder="Enter Your Name"/>
-<input type="text" v-model="email" placeholder="Enter Your Email"/>
-<input type="password" v-model="password" placeholder="Enter Your Password"/>
-<button v-on:click="signup" class="button">Sign Up</button>
-</div>
-
-
+  <img class="logo" src="../assets/logo.jpg" alt="" />
+  <h1>SignUp</h1>
+  <div class="container">
+    <div class="register">
+      <input type="text" v-model="name" placeholder="Enter Your Name" />
+      <input type="text" v-model="email" placeholder="Enter Your Email" />
+      <input
+        type="password"
+        v-model="password"
+        placeholder="Enter Your Password"
+      />
+      <button v-on:click="signup" class="button">Sign Up</button>
     </div>
-
-
+  </div>
 </template>
 <script>
-
-
-import axios from 'axios'
+import axios from "axios";
 export default {
-    name:'SignUp',
-    data()
-    {
-        return{
-            name:'',
-            email:'',
-            password:''
-        }
-        
+  name: "SignUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async signup() {
+      let result = await axios.post("http://localhost:3000/users", {
+        email: this.email,
+        password: this.password,
+        name: this.name,
+      });
+      console.warn(result);
+      if (result.status == 201) {
+        localStorage.setItem("user-info", JSON.stringify(result.data));
+        this.$router.push({ name: "Home" });
+      }
     },
-    methods:{
-            async signup()
-            {
-                let result = await axios.post("http://localhost:3000/users",{
-                    email:this.email,
-                    password:this.password,
-                    name:this.name
-                });
-                console.warn(result);
-                if(result.status==201)
-                {
-                    alert("sign up done");
-                    localStorage.setItem("user-info",JSON.stringify(result.data))
-                }
-            }
-        }
-}
+  },
+};
 </script>
 
 <style>
-.logo{
-    width: 200px;
+.logo {
+  width: 200px;
 }
-.register input{
-    font-family: 'Trebuchet MS';
-    width:300px;
-    display: block;
-    height: 20px;
-    padding-left: 15px;
-    margin-bottom:30px;
-    margin-right: auto;
-    margin-left: auto;
-    border: 3px solid orange;
+.register input {
+  font-family: "Trebuchet MS";
+  width: 300px;
+  display: block;
+  height: 20px;
+  padding-left: 15px;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 3px solid orange;
 }
-.button{
-    border: 3px solid skyblue;
-    width: 320px;
-    height: 40px;
-    background: skyblue;
-    color: white;
-    cursor: pointer;
+.button {
+  border: 3px solid skyblue;
+  width: 320px;
+  height: 40px;
+  background: skyblue;
+  color: white;
+  cursor: pointer;
 }
-
 </style>
-
